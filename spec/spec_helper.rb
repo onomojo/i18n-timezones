095ef2769d4ba58b1d4bfdc299b99e455e4ef147
Brian McQuay <brian@onomojo.com>
2012-04-02 20:32:59 -0600

    fixing tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,21 @@ ENV["RAILS_ENV"] = "test"
 require 'rspec'
 require 'i18n-spec'
 require 'i18n/core_ext/hash'
-require 'active_support/core_ext/kernel/reporting'
-require 'support/fake_app'
+require 'active_support'
+require 'rails/all'
+require 'i18n-timezones'
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.fail_fast = true
 end
+
+module Config
+  class Application < ::Rails::Application
+    # configuration here if needed
+    config.active_support.deprecation = :stderr
+  end
+end
+ 
+# Initialize the application
+Config::Application.initialize!
--- spec/unit/translations_spec.rb
+++ spec/unit/translations_spec.rb
@@ -1,10 +1,11 @@
-# coding: utf-8
-
 require 'spec_helper'
 
-Dir.glob('rails/locale/*.yml') do |locale_file|
+Dir.glob("rails/locale/*.yml") do |locale_file|
   describe "a i18n-timezones #{locale_file} locale file" do
     it_behaves_like 'a valid locale file', locale_file
-    it { locale_file.should be_a_subset_of('rails/locale/en.yml') }
+    I18n.locale = locale_file.split('/').last.split('.').first
+    ActiveSupport::TimeZone.all.each do |zone|
+      it { I18n.t(zone.name, :scope => :timezones).should_not == zone }
+    end
   end
 end
