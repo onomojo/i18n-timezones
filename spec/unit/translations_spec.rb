ae04fa7b9dde2eda0f3ecd85e2cc625121cff0c1
Randy Antler <randy@kudoway.com>
2021-10-21 16:56:14 -0700

    Clean up existing tests

--- spec/unit/translations_spec.rb
+++ spec/unit/translations_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
-Dir.glob("rails/locale/*.yml") do |locale_file|
-  describe "a i18n-timezones #{locale_file} locale file" do
+Dir.glob('rails/locale/*.yml') do |locale_file|
+  describe 'a i18n-timezones #{locale_file} locale file' do
+    let(:options) { {:scope => :timezones, :separator => "\001"} }
+
     it_behaves_like 'a valid locale file', locale_file
-    I18n.locale = locale_file.split('/').last.split('.').first
+
+    before { I18n.locale = locale_file.split('/').last.split('.').first }
+
     ActiveSupport::TimeZone.all.each do |zone|
-      p I18n.t(zone.name, :scope => :timezones, :separator => "\001") if I18n.t(zone.name, :scope => :timezones, :separator => "\001").include?('translation missing')
-      it { expect(I18n.t(zone.name, :scope => :timezones, :separator => "\001")).not_to eq(zone) }
-      it { expect(I18n.t(zone.name, :scope => :timezones, :separator => "\001").include?('translation missing')).to eq(false) }
+      it { expect(I18n.t(zone.name, options)).not_to eq(zone) }
+      it { expect(I18n.t(zone.name, options).include?('translation missing')).to eq(false) }
     end
   end
 end
